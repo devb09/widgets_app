1b532b49a62ab3931cff205d8c2468904a5fa96c
brn091 <brn091dev@gmail.com>
2023-07-03 00:14:29 -0500

    home screen page

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter + Matterial 3'),),
+      appBar: AppBar(
+        title: const Text('Flutter + Matterial 3'),
+      ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+ 
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuTiems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuTiems[index];
+        return _MenuListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _MenuListTile extends StatelessWidget {
+  const _MenuListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(menuItem.title),
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.chevron_right_rounded),
+      subtitle: Text(menuItem.subTitle),
     );
   }
 }
