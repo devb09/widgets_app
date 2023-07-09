f9853a48bbc556a6ddef7d8765bd21d5f86cd74e
brn091 <brn091dev@gmail.com>
2023-07-08 22:34:14 -0500

    elevations

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+final cards = <Map<String,dynamic>>[
+  {'elevation': 0.0,'label':'Elevation 0'},
+  {'elevation': 1.0,'label':'Elevation 1'},
+  {'elevation': 2.0,'label':'Elevation 2'},
+  {'elevation': 3.0,'label':'Elevation 3'},
+  {'elevation': 4.0,'label':'Elevation 4'},
+  {'elevation': 5.0,'label':'Elevation 5'},
+];
+
 class CardsScreen extends StatelessWidget {
 
   static const String name = 'crads_screen';
