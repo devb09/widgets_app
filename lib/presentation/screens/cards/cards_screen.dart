b2f6435d3c66385b98b0c330d33a4ea449cd5f9b
brn091 <brn091dev@gmail.com>
2023-07-08 22:53:43 -0500

    cards elevated

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
-final cards = <Map<String,dynamic>>[
-  {'elevation': 0.0,'label':'Elevation 0'},
-  {'elevation': 1.0,'label':'Elevation 1'},
-  {'elevation': 2.0,'label':'Elevation 2'},
-  {'elevation': 3.0,'label':'Elevation 3'},
-  {'elevation': 4.0,'label':'Elevation 4'},
-  {'elevation': 5.0,'label':'Elevation 5'},
+final cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
 ];
 
 class CardsScreen extends StatelessWidget {
-
   static const String name = 'crads_screen';
 
   const CardsScreen({super.key});
@@ -19,8 +18,9 @@ class CardsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _CardView(),
+        title: Text('Card view'),
       ),
+      body: _CardView(),
     );
   }
 }
@@ -32,6 +32,43 @@ class _CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Cards');
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map((card) => _CardType1(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                    onPressed: () {}, icon: Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
