858e4c59190dfb109041d4c7be1b8577520984e0
brn091 <brn091dev@gmail.com>
2023-07-17 23:37:57 -0500

    progress in menu

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,9 @@ const appMenuTiems = <MenuItem>[
       subTitle: 'Un contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'ProgessIndicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_outlined),
 ];
--- lib/presentation/screens/progess/progess_screen.dart
+++ lib/presentation/screens/progess/progess_screen.dart
@@ -7,6 +7,38 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicator'),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 30,
+          ),
+          Text('Circular Progress indicator'),
+          SizedBox(
+            height: 10,
+          ),
+          CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black45,
+          )
+        ],
+      ),
+    );
   }
 }
