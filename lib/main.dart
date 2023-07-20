6dc6879b19cd624afe79c705782589583c48085c
brn091 <brn091dev@gmail.com>
2023-07-19 22:10:33 -0500

    flutter dialog licenses

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Flutter Widgets',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).getTheme(),
       // home: HomeScreen(),
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -8,12 +8,12 @@ class SnackbarScreen extends StatelessWidget {
   void showCustomSnackbar(BuildContext context) {
     ScaffoldMessenger.of(context).clearSnackBars();
     final snackbar = SnackBar(
-      content: Text('Hola mundo'),
+      content: const Text('Hola mundo'),
       action: SnackBarAction(
         label: 'Ok!',
         onPressed: () {},
       ),
-      duration: Duration(seconds: 2),
+      duration: const Duration(seconds: 2),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
@@ -21,10 +21,27 @@ class SnackbarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Snackbars')),
+      appBar: AppBar(title: const Text('Snackbars')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(context: context, children: [
+                    const Text(
+                        'Lorem enim fugiat anim non officia sunt non laboris laboris. Ex velit nisi consequat aute est do do Lorem pariatur reprehenderit ipsum culpa anim nisi. Irure quis amet labore esse ea minim sunt esse irure non aute. Labore anim deserunt non magna quis duis dolor duis. Consequat dolore consectetur amet excepteur occaecat irure et Lorem est nostrud sunt cillum commodo sint. Non eu irure Lorem enim sit amet voluptate enim non cupidatat irure enim. Excepteur irure ut est esse eu id sint velit nisi adipisicing consequat Lorem sit est.')
+                  ]);
+                },
+                child: const Text('Licensias usadas')),
+            FilledButton.tonal(
+                onPressed: () {}, child: const Text('Mostrar dialogo')),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
-        label: Text('Mostrar snackbar'),
-        icon: Icon(Icons.remove_red_eye),
+        label: const Text('Mostrar snackbar'),
+        icon: const Icon(Icons.remove_red_eye),
         onPressed: () => showCustomSnackbar(context),
       ),
     );
